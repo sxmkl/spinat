d88955faa322c216cf80e1f3ae830686fd0c8080
sxmkl <142799111+sxmkl@users.noreply.github.com>
2025-02-22 20:39:44 +0800

    modified:   README.md 	new file:   rewrite/reddit.js

---
+++ rewrite/reddit.js
@@ -0,0 +1,24 @@
+/* 
+[rewrite_remote]
+^https?:\/\/gql-fed\.reddit\.com url script-response-body https://github.com/sxmkl/spinat/rewrite/reddit.js
+
+[mitm]
+hostname=gql-fed.reddit.com
+*/
+
+
+
+try {
+  const obj = JSON.parse($response.body);
+
+  let elements = obj.data?.homeV3?.elements;
+  elements && (elements.edges = elements.edges?.filter(e =>
+    !e.node?.adPayload
+  ) || []);
+  
+  obj.data?.children?.commentsPageAds && (obj.data.children.commentsPageAds = []);
+  
+  $done({body: JSON.stringify(obj)});
+} catch {
+  $done({});
+}
