a3ae5db6bfb368c5217ba39ae248974bd02abd8e
sxmkl <142799111+sxmkl@users.noreply.github.com>
2025-02-23 04:10:28 +0800

    modified:   rewrite/reddit.js

--- rewrite/reddit.js
+++ rewrite/reddit.js
@@ -5,20 +5,29 @@
 [mitm]
 hostname=gql-fed.reddit.com
 */
-
-
-
+const obj = JSON.parse($response.body);
+const data = obj.data;
+let e;
 try {
-  const obj = JSON.parse($response.body);
 
-  let elements = obj.data?.homeV3?.elements;
-  elements && (elements.edges = elements.edges?.filter(e =>
-    !e.node?.adPayload
-  ) || []);
-  
-  obj.data?.children?.commentsPageAds && (obj.data.children.commentsPageAds = []);
-  
+  if (e = data?.homeV3?.elements) {
+    e.edges = e.edges?.filter(edge =>
+      !edge.node?.adPayload
+    ) || [];
+  }
+
+  else if (data?.children?.commentsPageAds) {
+    data.children.commentsPageAds = [];
+  }
+
+  else if (e = data?.subredditInfoByName?.elements) {
+    e.edges = e.edges?.filter(edge =>
+      !edge.node?.__typename?.startsWith("Ad")
+    ) || [];
+  }
+
   $done({body: JSON.stringify(obj)});
-} catch {
+} catch (error) {
+  console.log('Error:', error);
   $done({});
 }
