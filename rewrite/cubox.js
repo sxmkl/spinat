cde6f02f4df70d51fa75c648d756cdc5a200e2fc
sxmkl <142799111+sxmkl@users.noreply.github.com>
2025-03-13 19:16:25 +0800

    new file:   rewrite/cubox.js 	modified:   rewrite/olist.js 	new file:   rewrite/pre.js

---
+++ rewrite/cubox.js
@@ -0,0 +1,25 @@
+/* 
+^https:\/\/cubox\.(pro|cc)\/c\/api\/user url script-response-body  https://raw.githubusercontent.com/sxmkl/spinat/main/rewrite/cubox.js
+hostname = cubox.pro,cubox.cc
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+
+const paths=["/userInfo","/userPay"]
+
+
+if (paths.some(path => url.includes(path))){
+    obj.data.level = 1;
+    obj.data.expireTime = "2099-09-12T23:50:23+08:00";
+    obj.data.isExpire = false;
+    obj.data.active = true;
+    obj.data.payTime = 1660006006;
+
+    body = JSON.stringify(obj);
+}
+
+
+$done({body});
---
+++ rewrite/pre.js
@@ -0,0 +1,29 @@
+/* 
+[rewrite_local]
+^https?:\/\/(i|market)\.waimai\.meituan\.com\/vp\/magical\/exchange\/pre url script-response-body https://n1t6l90p-2222.asse.devtunnels.ms/pre.js
+[mitm]
+hostname = *.waimai.meituan.com
+ */
+
+
+const obj = JSON.parse($response.body);
+const targetCouponGroups = obj.data.targetCouponGroups;
+
+let inflateInfo = {};
+
+targetCouponGroups.forEach((group) => {
+	let groupName = group.couponBizGroupName;
+	inflateInfo[groupName] = [];
+	group.targetCoupons.forEach((coupon) => {
+		let limit = (coupon.targetCouponRealAmountLimit / 100) || "";
+		let amount = coupon.targetCouponRealAmount / 100;
+		inflateInfo[groupName].push(`${limit}-${amount}`);
+		coupon.targetCouponAmount = "" + coupon.targetCouponRealAmount / 100;
+	});
+
+});
+
+console.log(JSON.stringify(inflateInfo, null, 2));
+
+$done({body: JSON.stringify(obj)});
+
